61d00058502282f6681ceb490be7453b1d451528
kai-morich <mail@kai-morich.de>
2021-09-21 19:22:06 +0200

    public

--- testlib/src/main/java/de/kai_morich/testlib/Lib.java
+++ testlib/src/main/java/de/kai_morich/testlib/Lib.java
@@ -3,5 +3,5 @@ package de.kai_morich.testlib;
 import androidx.annotation.NonNull;
 
 public class Lib {
-    static int get(@NonNull Integer i) {return i+1;}
+    public static int get(@NonNull Integer i) {return i+2;}
 }
